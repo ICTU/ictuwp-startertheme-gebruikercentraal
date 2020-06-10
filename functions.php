ed9cc81cfdec94d9ba81471e0fa23ef2952b5529
Paul van Buuren <paul@wbvb.nl>
2020-06-10 21:25:22 +0200

    menu toegevoegd

--- functions.php
+++ functions.php
@@ -1,12 +1,19 @@
 <?php
 /**
- * ictuwp-theme-gebruikercentraal
- * https://github.com/timber/starter-theme
+ * ictuwp-theme-gc2020
+ * https://github.com/ICTU/ictuwp-theme-gc2020
  *
  * @package  WordPress
  * @subpackage  Timber
  * @since   Timber 0.1
  */
+
+define( 'CHILD_THEME_VERSION', '5.0.1' );
+define( 'ID_MAINCONTENT', 'maincontent' );
+define( 'ID_MAINNAV', 'mainnav' );
+define( 'ID_ZOEKEN', 'zoeken' );
+define( 'ID_SKIPLINKS', 'skiplinks' );
+
 
 /**
  * If you are installing Timber as a Composer dependency in your theme, you'll need this block
@@ -27,17 +34,18 @@ if ( ! class_exists( 'Timber' ) ) {
 
 	add_action(
 		'admin_notices',
-		function() {
+		function () {
 			echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
 		}
 	);
 
 	add_filter(
 		'template_include',
-		function( $template ) {
+		function ( $template ) {
 			return get_stylesheet_directory() . '/static/no-timber.html';
 		}
 	);
+
 	return;
 }
 
@@ -57,20 +65,38 @@ Timber::$autoescape = false;
  * We're going to configure our theme inside of a subclass of Timber\Site
  * You can move this to its own file and include here via php's include("MySite.php")
  */
-class StarterSite extends Timber\Site {
+class GebruikerCentraalTheme extends Timber\Site {
 	/** Add timber support. */
 	public function __construct() {
+
+		// custom menu locations
+		add_action( 'init', array( $this, 'register_my_menu' ) );
+
+		// translation support
+		add_action( 'after_setup_theme', array( $this, 'add_translation_support' ) );
+
+
 		add_action( 'after_setup_theme', array( $this, 'theme_supports' ) );
+
+		add_action( 'wp_enqueue_scripts', array( $this, 'gc_wbvb_add_css' ) );
+
 		add_filter( 'timber/context', array( $this, 'add_to_context' ) );
 		add_filter( 'timber/twig', array( $this, 'add_to_twig' ) );
-		add_action( 'init', array( $this, 'register_post_types' ) );
+
 		add_action( 'init', array( $this, 'register_taxonomies' ) );
 		parent::__construct();
 	}
-	/** This is where you can register custom post types. */
-	public function register_post_types() {
 
+	// define menu location and name
+	public function register_my_menu() {
+		register_nav_menu( 'primary', __( 'Main menu location', 'gctheme' ) );
 	}
+
+	function add_translation_support() {
+		load_theme_textdomain( 'gctheme', get_template_directory() . '/languages' );
+	}
+
+
 	/** This is where you can register custom taxonomies. */
 	public function register_taxonomies() {
 
@@ -81,11 +107,16 @@ class StarterSite extends Timber\Site {
 	 * @param string $context context['this'] Being the Twig's {{ this }}.
 	 */
 	public function add_to_context( $context ) {
-		$context['foo']   = 'bar';
-		$context['stuff'] = 'I am a value set in your functions.php file';
-		$context['notes'] = 'These values are available everytime you call Timber::context();';
-		$context['menu']  = new Timber\Menu();
-		$context['site']  = $this;
+
+		$context['menu']        = new Timber\Menu( 'primary' );
+		$context['site']        = $this;
+		$context['site_name']   = ( get_bloginfo( 'name' ) ? get_bloginfo( 'name' ) : 'Gebruiker Centraal' );
+		$context['site_slogan'] = ( get_bloginfo( 'description' ) ? get_bloginfo( 'description' ) : null );
+
+		$blog_id = get_current_blog_id();
+
+		$context['logo']        = get_stylesheet_directory_uri() . '/static/img/logo/od.svg';
+
 		return $context;
 	}
 
@@ -149,6 +180,7 @@ class StarterSite extends Timber\Site {
 	 */
 	public function myfoo( $text ) {
 		$text .= ' bar!';
+
 		return $text;
 	}
 
@@ -159,9 +191,88 @@ class StarterSite extends Timber\Site {
 	public function add_to_twig( $twig ) {
 		$twig->addExtension( new Twig\Extension\StringLoaderExtension() );
 		$twig->addFilter( new Twig\TwigFilter( 'myfoo', array( $this, 'myfoo' ) ) );
+
 		return $twig;
 	}
 
+	function gc_wbvb_add_css() {
+
+		$dependencies = array();
+// /wp-content/themes/gc-pages-html/dist/css/gc-theme.css
+// /wp-content/themes/gebruiker-centraal/css/gc-style.css
+
+
+		wp_enqueue_style(
+			'gc-fonts',
+			get_stylesheet_directory_uri() . '/static/fonts/fonts.css',
+			$dependencies,
+			'',
+			'all'
+		);
+		wp_enqueue_style(
+			ID_SKIPLINKS,
+			get_stylesheet_directory_uri() . '/static/dist/css/gc-theme.css',
+			$dependencies,
+			CHILD_THEME_VERSION,
+			'all'
+		);
+
+		$custom_css = '
+	ul#' . ID_SKIPLINKS . ', ul#' . ID_SKIPLINKS . ' li {
+		list-style-type: none;
+		list-style-image: none;
+		padding: 0;
+		margin: 0;
+	}
+	ul#' . ID_SKIPLINKS . ' li {
+		background: none;
+	}
+	#' . ID_SKIPLINKS . ' li a {
+		position: absolute;
+		top: -1000px;
+		left: 50px;
+	}
+	#' . ID_SKIPLINKS . ' li a:focus {
+		left: 6px;
+		top: 7px;
+		height: auto;
+		width: auto;
+		display: block;
+		font-size: 14px;
+		font-weight: 700;
+		padding: 15px 23px 14px;
+		background: #f1f1f1;
+		color: #21759b;
+		z-index: 100000;
+		line-height: normal;
+		text-decoration: none;
+		-webkit-box-shadow: 0 0 2px 2px rgba(0,0,0,.6);
+		box-shadow: 0 0 2px 2px rgba(0,0,0,.6)
+	}
+
+	#' . ID_MAINNAV . ':focus {
+		position: relative;
+		z-index: 100000;
+	}
+
+	#' . ID_MAINNAV . ' a:focus {
+		position: relative;
+		z-index: 100000;
+		color: #fff;
+	}
+
+
+	#' . ID_ZOEKEN . ':focus label {
+		position: relative;
+		left: 0;
+		top: 0;
+	}';
+
+		wp_add_inline_style( ID_SKIPLINKS, $custom_css );
+
+	}
+
+
 }
 
-new StarterSite();
+new GebruikerCentraalTheme();
