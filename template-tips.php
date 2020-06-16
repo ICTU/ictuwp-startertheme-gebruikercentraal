79bb898ba23dcebead68d8ca6f988006e2db91f3
Paul van Buuren <paul@wbvb.nl>
2020-06-16 15:20:40 +0200

    eerste versie voor apart paginatemplate voor een pagina met enkele tips

---
+++ template-tips.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template Name: Tips
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+
+$context = Timber::context();
+
+$timber_post     = new Timber\Post();
+$context['post'] = $timber_post;
+$context['tips'] = array( 'yo tips');
+
+
+
+Timber::render( array( 'page-alle-tips.twig', 'page.twig' ), $context );
