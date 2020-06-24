083a0b97ac8ad272ef525cfbc604ab97782cd3f2
Paul van Buuren <paul@wbvb.nl>
2020-06-24 17:19:18 +0200

    Themavariabelen variabel pad geven
    logo's toegevoegd voor tests. vertalingen bijgewerkt

--- functions.php
+++ functions.php
@@ -12,8 +12,10 @@ define( 'CHILD_THEME_VERSION', '5.0.1' );
 define( 'ID_MAINCONTENT', 'maincontent' );
 define( 'ID_MAINNAV', 'mainnav' );
 define( 'ID_ZOEKEN', 'zoeken' );
-define( 'ID_SKIPLINKS', 'skiplinks' );
+define( 'ID_SKIPLINKS', 'skiplinksjes' );
 
+define( 'DEFAULTFLAVOR', 'GC' );
+define( 'FLAVORSCONFIG', 'flavors_config.json' );
 
 // Custom Post Types
 if ( ! defined( 'GC_TIP_CPT' ) ) {
@@ -30,6 +32,7 @@ if ( ! defined( 'GC_TIPVRAAG' ) ) {
 if ( ! defined( 'OD_CITAATAUTEUR' ) ) {
 	define( 'OD_CITAATAUTEUR', 'tipgever' );
 }
+
 
 // constants for image sizes
 define( 'BLOG_SINGLE_MOBILE', 'blog-single-mobile' );
@@ -97,6 +100,10 @@ Timber::$autoescape = false;
  * You can move this to its own file and include here via php's include("MySite.php")
  */
 class GebruikerCentraalTheme extends Timber\Site {
+
+	// contains configuration settings
+	protected $configuration;
+
 	/** Add timber support. */
 	public function __construct() {
 
@@ -122,6 +129,7 @@ class GebruikerCentraalTheme extends Timber\Site {
 		add_action( 'theme_page_templates', array( $this, 'activate_deactivate_page_templates' ) );
 
 		parent::__construct();
+
 	}
 
 	// define menu location and name
@@ -146,18 +154,48 @@ class GebruikerCentraalTheme extends Timber\Site {
 	 */
 	public function add_to_context( $context ) {
 
-		$blog_id = get_current_blog_id();
+		// read configuration json file
+		$configfile    = file_get_contents( trailingslashit( get_stylesheet_directory() ) . FLAVORSCONFIG );
+		$configfile    = json_decode( $configfile, true );
+		$theme_options = get_option( 'gc2020_theme_options' );
+		$flavor        = DEFAULTFLAVOR; // default, tenzij er een smaakje is gekozen
+		if ( isset( $theme_options['flavor_select'] ) ) {
+			$flavor = $theme_options['flavor_select'];
+		}
+		if ( isset( $configfile[ DEFAULTFLAVOR ] ) ) {
+			$defaultsettings = $configfile[ DEFAULTFLAVOR ];
+		} else {
+			// iemand heeft een typvaud gemaakt en de in dit bestand hier
+			// gedefinieerde default staat niet in het json-bestand.
+			// Beetje jammer, maar dan nemen we -op hoop van zegen- het
+			// eerste setje configuratieregels
+			$defaultsettings = reset( $configfile );
+		}
+
+		if ( isset( $configfile[ $flavor ] ) ) {
+			// admin has chosen a flavor (any flavor), so let's
+			// merge the configuration of chosen flavor with the default settings
+			$this->configuration = wp_parse_args( $configfile[ $flavor ], $defaultsettings );
+
+		} else {
+			// no flavor chosen, so set the configuration to the default settings
+			$this->configuration = $defaultsettings;
+		}
 
 		$context['menu']                = new Timber\Menu( 'primary' );
 		$context['footermenu']          = new Timber\Menu( 'footermenu' );
 		$context['site']                = $this;
 		$context['site_name']           = ( get_bloginfo( 'name' ) ? get_bloginfo( 'name' ) : 'Gebruiker Centraal' );
-		$context['site_slogan']         = ( get_bloginfo( 'description' ) ? get_bloginfo( 'description' ) : null );
-		$context['logo']                = get_stylesheet_directory_uri() . '/flavors/optimaaldigitaal/logo.svg';
+		$context['alt_logo']            = sprintf( _x( 'Logo %s', 'Alt-tekst op logo', 'gctheme' ), get_bloginfo( 'name' ) );
 		$context['sprite_od']           = get_stylesheet_directory_uri() . '/assets/images/sprites/optimaal-digitaal/defs/svg/sprite.defs.svg';
 		$context['sprite_steps']        = get_stylesheet_directory_uri() . '/assets/images/sprites/stepchart/defs/svg/sprite.defs.svg';
 		$context['footer_widget_left']  = Timber::get_widgets( 'footer_widget_left' );
 		$context['footer_widget_right'] = Timber::get_widgets( 'footer_widget_right' );
+		$context['site_logo']           = get_stylesheet_directory_uri() . $this->configuration['site_logo'];
+		$context['skiplinks_id']        = ID_SKIPLINKS;
+		$context['maincontent_id']      = 'maincontent';
+		$context['mainnav_id']          = 'menu-primary';
+
 
 		return $context;
 	}
@@ -252,94 +290,116 @@ class GebruikerCentraalTheme extends Timber\Site {
 
 	public function gc_wbvb_add_css() {
 
-
-		$dependencies = array();
-		$versie       = CHILD_THEME_VERSION;
-		$infooter     = false;
-		$gc_theme = get_option('gc2020_theme_options');
-
+		$versie = CHILD_THEME_VERSION;
 
 		if ( WP_DEBUG ) {
 			$versie = strtotime( "now" );
 		}
 
-		wp_enqueue_script( 'main-min', get_stylesheet_directory_uri() . '/assets/js/main-min.js', $dependencies, $versie, $infooter );
+		if ( is_array( $this->configuration['jsfiles'] ) ) {
 
-		$dependencies = array();
+			foreach ( $this->configuration['jsfiles'] as $key => $value ) {
+
+				$dependencies = $value['dependencies'];
+
+				if ( $value['version'] ) {
+					$versie = $value['version'];
+				}
+
+				wp_enqueue_script(
+					$value['handle'],
+					get_stylesheet_directory_uri() . $value['file'],
+					$dependencies,
+					$versie,
+					$value['infooter']
+				);
+			}
+		}
+
+		$skiplinkshandle = ID_SKIPLINKS;
+
+		if ( is_array( $this->configuration['cssfiles'] ) ) {
+
+			foreach ( $this->configuration['cssfiles'] as $key => $value ) {
+
+				$dependencies = $value['dependencies'];
+
+				if ( $value['version'] ) {
+					$versie = $value['version'];
+				}
+
+				wp_enqueue_style(
+					$value['handle'],
+					get_stylesheet_directory_uri() . $value['file'],
+					$dependencies,
+					$versie,
+					'all'
+				);
+				$skiplinkshandle = $value['handle'];
+
+			}
+
+			$custom_css = '
+				ul#' . ID_SKIPLINKS . ', ul#' . ID_SKIPLINKS . ' li {
+					list-style-type: none;
+					list-style-image: none;
+					padding: 0;
+					margin: 0;
+				}
+				ul#' . ID_SKIPLINKS . ' li {
+					background: none;
+				}
+				#' . ID_SKIPLINKS . ' li a {
+					position: absolute;
+					top: -1000px;
+					left: 50px;
+				}
+				#' . ID_SKIPLINKS . ' li a:focus {
+					left: 6px;
+					top: 7px;
+					height: auto;
+					width: auto;
+					display: block;
+					font-size: 14px;
+					font-weight: 700;
+					padding: 15px 23px 14px;
+					background: #f1f1f1;
+					color: #21759b;
+					z-index: 100000;
+					line-height: normal;
+					text-decoration: none;
+					-webkit-box-shadow: 0 0 2px 2px rgba(0,0,0,.6);
+					box-shadow: 0 0 2px 2px rgba(0,0,0,.6)
+				}
+
+				#' . ID_MAINNAV . ':focus {
+					position: relative;
+					z-index: 100000;
+				}
+
+				#' . ID_MAINNAV . ' a:focus {
+					position: relative;
+					z-index: 100000;
+					color: #fff;
+				}
+
+				#' . ID_ZOEKEN . ':focus label {
+					position: relative;
+					left: 0;
+					top: 0;
+				}';
+
+			if ( $this->configuration['site_logo'] ) {
+				$custom_css .= "
+				 .gc-site-footer-widget {
+					background-image: url('" . get_stylesheet_directory_uri() . $this->configuration['site_logo'] . "');
+				}";
+			}
 
 
-		// TODO : verwijzen naar de relevante CSS
+			wp_add_inline_style( $skiplinkshandle, $custom_css );
 
-		wp_enqueue_style(
-			'gc-fonts',
-			get_stylesheet_directory_uri() . '/assets/fonts/fonts.css',
-			$dependencies,
-			'',
-			'all'
-		);
-
-		// get_stylesheet_directory_uri() . '/flavors/'.$gc_theme["theme_select"].'assets/css/od-theme.css',
-		wp_enqueue_style(
-			ID_SKIPLINKS,
-			get_stylesheet_directory_uri() . '/flavors/optimaaldigitaal/assets/css/od-theme.css',
-			$dependencies,
-			$versie,
-			'all'
-		);
-
-		$custom_css = '
-	ul#' . ID_SKIPLINKS . ', ul#' . ID_SKIPLINKS . ' li {
-		list-style-type: none;
-		list-style-image: none;
-		padding: 0;
-		margin: 0;
-	}
-	ul#' . ID_SKIPLINKS . ' li {
-		background: none;
-	}
-	#' . ID_SKIPLINKS . ' li a {
-		position: absolute;
-		top: -1000px;
-		left: 50px;
-	}
-	#' . ID_SKIPLINKS . ' li a:focus {
-		left: 6px;
-		top: 7px;
-		height: auto;
-		width: auto;
-		display: block;
-		font-size: 14px;
-		font-weight: 700;
-		padding: 15px 23px 14px;
-		background: #f1f1f1;
-		color: #21759b;
-		z-index: 100000;
-		line-height: normal;
-		text-decoration: none;
-		-webkit-box-shadow: 0 0 2px 2px rgba(0,0,0,.6);
-		box-shadow: 0 0 2px 2px rgba(0,0,0,.6)
-	}
-
-	#' . ID_MAINNAV . ':focus {
-		position: relative;
-		z-index: 100000;
-	}
-
-	#' . ID_MAINNAV . ' a:focus {
-		position: relative;
-		z-index: 100000;
-		color: #fff;
-	}
-
-
-	#' . ID_ZOEKEN . ':focus label {
-		position: relative;
-		left: 0;
-		top: 0;
-	}';
-
-		wp_add_inline_style( ID_SKIPLINKS, $custom_css );
-
+		}
 	}
 
 	/**
@@ -400,7 +460,6 @@ function insert_breadcrumb() {
 }
 
 
-
 /*
  * filter body class
  */
@@ -413,15 +472,14 @@ function my_body_classes( $classes ) {
 
 	$classes[] = 'meh';
 
-if ( is_page() ) {
+	if ( is_page() ) {
 
-	$template = basename( get_page_template() );
-	if ( 'template-alle-tips.php' === $template ) {
-		$classes[] = 'page--type-overview page--overview-archive';
-	}
+		$template = basename( get_page_template() );
+		if ( 'template-alle-tips.php' === $template ) {
+			$classes[] = 'page--type-overview page--overview-archive';
+		}
 
-}
-elseif ( is_singular( GC_TIP_CPT ) ) {
+	} elseif ( is_singular( GC_TIP_CPT ) ) {
 
 		$classes[] = 'page page--type-tipkaart';
 		$taxonomie = get_the_terms( $post, GC_TIPTHEMA );
@@ -445,8 +503,6 @@ elseif ( is_singular( GC_TIP_CPT ) ) {
 }
 
 
-
-
 /*
  * filter for breadcrumb
  */
@@ -458,8 +514,7 @@ function unbox_yoast_seo_breadcrumb_append_link( $links ) {
 	if ( is_home() || is_front_page() ) {
 		// geen breadcrumb op de homepage
 		return array();
-	}
-	elseif ( is_singular( GC_TIP_CPT ) ) {
+	} elseif ( is_singular( GC_TIP_CPT ) ) {
 		// uit siteopties de pagina ophalen die het overzicht is van alle links
 		$optionpage = get_field( 'op_welke_pagina_staat_het_overzicht_van_alle_tips', 'option' );
 
@@ -501,43 +556,47 @@ function unbox_yoast_seo_breadcrumb_append_link( $links ) {
 
 	return $links;
 }
+
 /**
  * Add our Customizer content
  */
-function gc2020_customize_register($wp_customize){
+function gc2020_customize_register( $wp_customize ) {
 
-	$wp_customize->add_section('gc2020_theme', array(
-		'title'    => __('GC Theme opties', 'ictuwp-theme-gc2020'),
-		'description' => 'Selecteer hier het thema voor deze subsite',
-		'priority' => 120,
-	));
+	$wp_customize->add_section( 'gc2020_theme', array(
+		'title'       => _x( 'Gebruiker Centraal Instellingen', 'customizer', 'gctheme' ),
+		'description' => _x( 'Selecteer hier het kleurenschema voor deze site. Je wijzigt hiermee de styling, logo en sommige functionaliteit van de site.', 'customizer', 'gctheme' ),
+		'priority'    => 60,
+	) );
 	//  =============================
 	//  = Select Box                =
 	//  =============================
-	$wp_customize->add_setting('gc2020_theme_options[theme_select]', array(
-		'default'        => 'GC',
-		'capability'     => 'edit_theme_options',
-		'type'           => 'option',
+	$wp_customize->add_setting( 'gc2020_theme_options[flavor_select]', array(
+		'default'    => DEFAULTFLAVOR,
+		'capability' => 'edit_theme_options',
+		'type'       => 'option',
+	) );
 
-	));
+	$flavors = array();
+
+	// read configuration json file
+	$configfile   = file_get_contents( trailingslashit( get_stylesheet_directory() ) . FLAVORSCONFIG );
+	$flavorsource = json_decode( $configfile, true );
+	foreach ( $flavorsource as $key => $value ) {
+		$flavors[ strtoupper( $key ) ] = $value['name'];
+	}
+
 	$wp_customize->add_control( 'example_select_box', array(
-		'settings' => 'gc2020_theme_options[theme_select]',
-		'label'   => 'Selecteer thema:',
-		'section' => 'gc2020_theme',
-		'type'    => 'select',
-		'choices'    => array(
-			'GC' => 'Gebruiker Centraal default theme',
-			'OD' => 'Optimaal Digitaal theme',
-
-		),
-	));
-
-
+		'settings' => 'gc2020_theme_options[flavor_select]',
+		'label'    => _x( 'Kies het kleurenschema', 'customizer', 'gctheme' ),
+		'section'  => 'gc2020_theme',
+		'type'     => 'select',
+		'choices'  => $flavors,
+	) );
 
 
 }
 
-add_action('customize_register', 'gc2020_customize_register');
+add_action( 'customize_register', 'gc2020_customize_register' );
 
 
 //========================================================================================================
