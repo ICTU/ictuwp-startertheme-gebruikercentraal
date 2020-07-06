0edb3e8d91a114e5679d20358fcc1346b2b4f46d
Paul van Buuren <paul@wbvb.nl>
2020-07-06 12:37:37 +0200

    Titel op template alle tips gecorrigeerd, filter tijdelijk weggehaald en ID toegevoegd

--- template-alle-tips.php
+++ template-alle-tips.php
@@ -8,23 +8,21 @@
  */
 
 
-$context = Timber::context();
-
-$timber_post          = new Timber\Post();
+$context              = Timber::context();
+$timber_post          = Timber::query_post();
 $context['post']      = $timber_post;
-$context['filters']   = array();
-$context['tipkaarts'] = array();
-$themakleuren         = array();
-$card                 = array();
-$context              = Timber::get_context();
+$context['filters']   = [];
+$context['tipkaarts'] = [];
+$themakleuren         = [];
+$card                 = [];
 
 // eerst alle tipthema's langs om de kleuren op te halen
-$args  = array(
+$args  = [
 	'taxonomy'   => GC_TIPTHEMA,
-	'hide_empty' => true,
+	'hide_empty' => TRUE,
 	'orderby'    => 'name',
 	'order'      => 'ASC',
-);
+];
 $terms = get_terms( $args );
 
 if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) {
@@ -37,11 +35,15 @@ if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) {
 		if ( $themakleur ) {
 
 			$themakleuren[ $term->term_id ] = $themakleur;
+			/*
+			 * TODO: filters weer terug zetten
+			 * filter array leeghouden, zodat frontend nog even zo schoon mogelijk blijft
 			$context['filters'][]           = array(
 				'id'    => $term->term_id,
 				'name'  => $term->name,
 				'class' => $themakleur,
 			);
+			*/
 
 		} else {
 			// kleur ontbreekt
@@ -49,16 +51,16 @@ if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) {
 	}
 }
 
-$args = array(
+$args = [
 	// Get post type project
 	'post_type'      => GC_TIP_CPT,
 	// Get all posts
 	'posts_per_page' => - 1,
 	'post_status'    => 'publish',
-	'orderby'        => array(
-		'date' => 'DESC'
-	)
-);
+	'orderby'        => [
+		'date' => 'DESC',
+	],
+];
 
 $the_query = new WP_Query( $args );
 
@@ -67,17 +69,17 @@ if ( $the_query->have_posts() ) {
 	while ( $the_query->have_posts() ) {
 
 		$the_query->the_post();
-		$card = array();
+		$card          = [];
 		$taxonomie     = get_the_terms( $post->ID, GC_TIPTHEMA );
 		$card['title'] = od_wbvb_custom_post_title( get_the_title() );
-		$card['nr']    = sprintf( _x( 'Tip %s', 'Label tip-ummer', 'gctheme' ), get_post_meta( $post->ID, 'tip-nummer', true ) );
+		$card['nr']    = sprintf( _x( 'Tip %s', 'Label tip-ummer', 'gctheme' ), get_post_meta( $post->ID, 'tip-nummer', TRUE ) );
 		$card['url']   = get_the_permalink();
-		$is_toptip     = get_post_meta( $post->ID, 'is_toptip', true );
+		$is_toptip     = get_post_meta( $post->ID, 'is_toptip', TRUE );
 		if ( $is_toptip ) {
-			$card['toptip'] = true;
+			$card['toptip']      = TRUE;
 			$card['toptiptekst'] = 'Toptip';
 		} else {
-			$card['toptip'] = false;
+			$card['toptip'] = FALSE;
 		}
 		if ( isset( $themakleuren[ $taxonomie[0]->term_id ] ) ) {
 			$card['category'] = $themakleuren[ $taxonomie[0]->term_id ];
@@ -92,15 +94,4 @@ if ( $the_query->have_posts() ) {
 wp_reset_postdata();
 
 
-/*
-"26": {
-	"title": "Meet en verbeter continu",
-    "category": "Procesaanpak",
-    "url": "tip-procesaanpak.php",
-    "toptip": "ja"
-  },
- *
- */
-
-
-Timber::render( array( 'template-alle-tips.twig', 'page.twig' ), $context );
+Timber::render( [ 'template-alle-tips.twig', 'page.twig' ], $context );
