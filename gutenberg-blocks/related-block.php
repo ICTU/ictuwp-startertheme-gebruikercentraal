c30841cc407e69bb84b36884b21f0ff66a54c370
Paul van Buuren <paul@wbvb.nl>
2020-07-07 13:55:00 +0200

    Gerelateerde items zijn nu ook een Gutenberg-block
    GB-block is ook geschikt voor het tonen van een tip als card

--- gutenberg-blocks/related-block.php
+++ gutenberg-blocks/related-block.php
@@ -1,6 +1,48 @@
 <?php
 
 
+add_action( 'acf/init', 'gb_add_related_block' );
+
+function gb_add_related_block() {
+
+	// Check function exists.
+	if ( function_exists( 'acf_register_block_type' ) ) {
+
+		// register a testimonial block.
+		acf_register_block_type( [
+			'name'            => 'gc/related',
+			'title'           => _x( 'GC gerelateerde content', 'Block titel', 'gctheme' ),
+			'description'     => _x( 'Tonen van gerelateerde content in een apart block', 'Block description', 'gctheme' ),
+			'render_callback' => 'gb_render_related_block',
+			'category'        => 'gc-blocks',
+			'icon'            => 'megaphone', // todo: eigen icon voor dit block
+			'keywords'        => [ 'link', 'text', 'image' ],
+		] );
+	}
+}
+
+
+function gb_render_related_block( $block, $content = '', $is_preview = FALSE ) {
+
+	$context = Timber::context();
+
+	// Store block values.
+	$context['block'] = $block;
+
+	// Store field values.
+	$context['fields'] = get_fields();
+
+	// Store $is_preview value.
+	$context['is_preview'] = $is_preview;
+
+	$context['related'] = related_block_get_data();
+
+	// Render the block.
+	Timber::render( 'sections/section-related.html.twig', $context );
+}
+
+
+
 /*
  * returns an array for the related section
  */
@@ -12,32 +54,58 @@ function related_block_get_data() {
 	if ( 'ja' === get_field( 'gerelateerde_content_toevoegen' ) ) {
 
 		$featured_posts = get_field( 'content_block_items' );
+		$themakleuren   = [];
 
 		if ( $featured_posts ):
 
 			foreach ( $featured_posts as $post ):
 
-				$item['title']  = get_the_title( $post );
+				$item['title'] = get_the_title( $post );
 
-				$item           = [];
-				$item['title']  = get_the_title( $post );
-				$item['descr']  = get_the_excerpt( $post );
-				$item['type']   = get_post_type( $post );
-				$item['url']    = get_the_permalink( $post );
-				$image          = get_the_post_thumbnail( $post->ID, 'large', [] );
-				$item['img']    = $image;
+				$item          = [];
+				$item['title'] = get_the_title( $post );
+				$item['descr'] = get_the_excerpt( $post );
+				$item['type']  = get_post_type( $post );
+				$item['url']   = get_the_permalink( $post );
+				$image         = get_the_post_thumbnail( $post->ID, 'large', [] );
+				$item['img']   = $image;
 
-				if ( 'post' ==  get_post_type( $post ) ) {
+				if ( 'tips' == get_post_type( $post ) ) {
 
-					$item['meta'][]  = [
-						'title' => 'date',
-						'descr' => get_the_time( get_option('date_format'), $post->ID),
-					];
+					// het is een tip
+					// eerst checken of we al alle themakleuren hebben
+					if ( ! $themakleuren ) {
+						$themakleuren = get_themakleuren();
+					}
 
-					$item['meta'][]  = [
+					$item['nr']     = sprintf( _x( 'Tip %s', 'Label tip-nummer', 'gctheme' ), get_post_meta( $post->ID, 'tip-nummer', TRUE ) );
+					$item['toptip'] = FALSE;
+					$is_toptip      = get_post_meta( $post->ID, 'is_toptip', TRUE );
+
+					if ( $is_toptip ) {
+						$item['toptip']      = TRUE;
+						$item['toptiptekst'] = 'Toptip';
+					}
+
+					$taxonomie = get_the_terms( $post->ID, GC_TIPTHEMA );
+
+					if ( isset( $themakleuren[ $taxonomie[0]->term_id ] ) ) {
+						$item['category'] = $themakleuren[ $taxonomie[0]->term_id ];
+					}
+
+
+				} elseif ( 'post' == get_post_type( $post ) ) {
+
+					$item['meta'][] = [
 						'title' => 'author',
 						'descr' => get_the_author_meta( 'display_name', $post->post_author ),
 					];
+
+					$item['meta'][] = [
+						'title' => 'date',
+						'descr' => get_the_time( get_option( 'date_format' ), $post->ID ),
+					];
+
 				}
 
 				$return['items'][] = $item;
@@ -46,9 +114,21 @@ function related_block_get_data() {
 
 		endif;
 
+		$columncounter = '2';
+
+		if ( count( $return['items'] ) < 2 ) {
+			$columncounter = '1';
+		} elseif ( count( $return['items'] ) === 4 ) {
+			$columncounter = '2';
+		} elseif ( count( $return['items'] ) > 2 ) {
+			$columncounter = '3';
+		}
+
+		$return['columncounter'] = $columncounter;
+
 		if ( $return['items'] ) {
-			$return['title'] = get_field( 'content_block_title' ) ? get_field( 'content_block_title' ) : _x( 'Gerelateerd', 'Titel boven gerelateerde links', 'gctheme' );
-			$return['desc']  = 'DESC HIER: ' . get_field( 'downloads_description' );
+			$return['description'] = get_field( 'content_block_description' ) ? get_field( 'content_block_description' ) : '';
+			$return['title']       = get_field( 'content_block_title' ) ? get_field( 'content_block_title' ) : '';
 		}
 
 	}
