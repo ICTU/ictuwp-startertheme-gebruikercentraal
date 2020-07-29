5fc3e1f9859a9ce578f5e79c16463d6043560ade
Mark van der Zande <mark.vanderzande@ictu.nl>
2020-07-29 16:07:26 +0200

    translate Kennisbank pugin
    Add translations files

--- plugin-activatie/plugins/kennisbank/minervakb/lib/blocks/controls/common/control-wrap/index.js
+++
@@ -1,9 +0,0 @@
-export const ControlWrap = ({ children, label, help }) => (
-    <div className="components-base-control">
-        <label className="components-base-control__label">{label}</label>
-        {children}
-        {help && (
-            <p className="components-base-control__help">{help}</p>
-        )}
-    </div>
-);
--- plugin-activatie/plugins/kennisbank/minervakb/lib/blocks/utils/index.js
+++
@@ -1,3 +0,0 @@
-export const getOption = (key, defaultValue = null) => window.MinervaKB.settings[key] || defaultValue;
-
-export const pluginUrl = (path) => window.MinervaKB.pluginUrl + path;
