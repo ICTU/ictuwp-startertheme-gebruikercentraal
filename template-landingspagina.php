85742b94724e38b6d6c9f372dd80e7339d75913c
Paul van Buuren <paul@wbvb.nl>
2020-08-12 20:26:17 +0200

    Tonen van inleiding op landingspagina

--- template-landingspagina.php
+++ template-landingspagina.php
@@ -9,10 +9,15 @@
 
 $context = Timber::context();
 
-$timber_post        = new Timber\Post();
-$context['post']    = $timber_post;
-$context['intro']    = 'haha intro';
+$timber_post     = new Timber\Post();
+$context['post'] = $timber_post;
 
+if ( get_field( 'post_inleiding' ) ) {
+	// ACF veld 'post_inleiding' is gevuld
+	$intro            = get_field( 'post_inleiding' );
+	$context['intro'] = wpautop( $intro );
+
+}
 
 
 Timber::render( array( 'page-landing.twig', 'page.twig' ), $context );
