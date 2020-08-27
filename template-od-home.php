080dd8f6ad2ff7bbe4e5572f32457e7a96790ca4
Paul van Buuren <paul@wbvb.nl>
2020-08-27 12:17:51 +0200

    events en blogposts toegevoegd aan homepage

--- template-od-home.php
+++ template-od-home.php
@@ -11,6 +11,7 @@
 $context         = Timber::context();
 $timber_post     = Timber::query_post();
 $context['post'] = $timber_post;
+$defaultimage    = get_template_directory_uri() . '/flavors/optimaaldigitaal/assets/images/od-default.jpg';
 
 if ( 'ja' === get_field( 'gerelateerde_content_toevoegen' ) ) {
 
@@ -27,8 +28,52 @@ if ( $spotlightblocks ) {
 
 }
 
+// events selecteren
+if ( class_exists( 'EM_Events' ) ) {
+
+	$args = array(
+		'limit' => 2,
+		'array' => true
+	);
+
+	$events = EM_Events::get( $args );
+
+	if ( $events ) {
+
+		foreach ( $events as $event ):
+			$item                 = array();
+			$startdate            = $event['event_start'];
+			$event_start_datetime = strtotime( $startdate );
+			$item['date']         = date_i18n( 'j M', $event_start_datetime );
+			$item['title']        = $event['event_name'];
+			$image                = get_the_post_thumbnail_url( $event['post_id'], 'large' );
+
+			if ( ! $image ) {
+				$item['img']     = $defaultimage;
+				$item['img_alt'] = '';
+			} else {
+				$item['img']     = $image;
+				$item['img_alt'] = get_post_meta( get_post_thumbnail_id( $post->ID ), '_wp_attachment_image_alt', true );
+			}
+
+
+			$context['actueel']['events']['items'][] = $item;
+
+		endforeach;
+
+		if( get_option('dbem_events_page') ) {
+			$context['actueel']['events']['cta']['url']   = get_permalink( get_option( 'dbem_events_page' ) );
+			$context['actueel']['events']['cta']['title'] = get_the_title( get_option( 'dbem_events_page' ) );
+			$context['actueel']['events']['cta']['url']   = get_permalink( get_option( 'dbem_events_page' ) );
+		}
+
+	}
+
+
+}
+
 // posts selecteren
-$maxnr_tips  = 4;
+$maxnr_tips  = ( 4 - count( $context['actueel']['events']['items'] ) );
 $args        = array(
 	'post_type'   => 'post',
 	'numberposts' => $maxnr_tips,
@@ -37,8 +82,7 @@ $args        = array(
 $relatedtips = new WP_Query( $args );
 if ( $relatedtips->have_posts() ) {
 
-	$counter      = 0;
-	$defaultimage = get_template_directory_uri() . '/flavors/optimaaldigitaal/assets/images/od-default.jpg';
+	$counter = 0;
 
 	while ( $relatedtips->have_posts() ) {
 		$relatedtips->the_post();
@@ -58,7 +102,7 @@ if ( $relatedtips->have_posts() ) {
 
 	}
 
-	if (  get_option( 'page_for_posts' ) ) {
+	if ( get_option( 'page_for_posts' ) ) {
 		$context['actueel']['blogs']['cta']['url']   = get_permalink( get_option( 'page_for_posts' ) );
 		$context['actueel']['blogs']['cta']['title'] = get_the_title( get_option( 'page_for_posts' ) );
 	}
@@ -68,25 +112,7 @@ if ( $relatedtips->have_posts() ) {
 }
 
 
-// events selecteren
-if ( class_exists( 'EM_Events' ) ) {
-	$events = EM_Events::output( array( 'limit' => 2 ) );
-
-	echo '<pre>';
-	var_dump( $events );
-	echo '</pre>';
-
-}
-
-if ( 22 === 33 ) {
-	Timber::render( [
-		'page-' . $timber_post->post_name . '.twig',
-		'page.twig',
-	], $context );
-
-} else {
-	Timber::render( [ 'od-home.html.twig', 'page.twig' ], $context );
-}
+Timber::render( [ 'od-home.html.twig', 'page.twig' ], $context );
 
 
 
