9314a05d71f3b134eda65761b034fad742117353
Paul van Buuren <paul@wbvb.nl>
2020-10-09 16:21:50 +0200

    versienummer opgehoogd om caching te voorkomen

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
  * @since   Timber 0.1
  */
 
-define( 'CHILD_THEME_VERSION', '5.0.13.e' );
+define( 'CHILD_THEME_VERSION', '5.0.13' );
 define( 'ID_MAINCONTENT', 'maincontent' );
 define( 'ID_MAINNAV', 'mainnav' );
 define( 'ID_ZOEKEN', 'zoeken' );
