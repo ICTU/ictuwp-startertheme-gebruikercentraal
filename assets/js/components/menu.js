de8d180c7e15791427c81a01f0559773f79e856c
Paul van Buuren <paul@wbvb.nl>
2020-11-10 09:55:37 +0100

    Merge branch 'development' into verbouwing-menu

--- assets/js/components/menu.js
+++ assets/js/components/menu.js
@@ -6,7 +6,7 @@
 // @package gebruiker-centraal
 // @author  Tamara de Haas, Paul van Buuren
 // @license GPL-2.0+
-// @version 5.0.20
+// @version 5.0.22.temp
 // @desc.   CTA-kleuren, a11y groen, sharing buttons optional, beeldbank CPT code separation.
 // @link    https://github.com/ICTU/gebruiker-centraal-wordpress-theme
 
@@ -207,7 +207,6 @@ else {
     var windowwidth = window.innerWidth;
     doNav(windowwidth);
   });
-
 
 }
 
