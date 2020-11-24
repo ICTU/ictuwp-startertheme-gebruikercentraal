c93ecf69009cac2a8fd308e54c9eb4ef507510be
Mark van der Zande <mark.vanderzande@ictu.nl>
2020-11-24 14:14:05 +0100

    filter OD

--- assets/js/components/filters.js
+++ assets/js/components/filters.js
@@ -22,4 +22,54 @@
   });
 
 // =========================================================================================================
+jQuery(document).ready(function() {
+(function() {
+  
+  var $imgs = $('.section .grid .tipkaart');
+  var $buttons = $('#buttons');
+  var tagged = {};
+  
+  $imgs.each(function(){
+    var img = this;
+    var tags = $(this).data('tags');
+    
+    if (tags) {
+      tags.split(',').forEach(function(tagName) {
+        if (tagged[tagName] == null) {
+          tagged[tagName] = []; 
+        }
+        tagged[tagName].push(img);
+      });
+    }
+  });
+// Buttons, ents, filters below...
+$('<button/>', {
+  text: 'Alle tipkaarten',
+  class: 'active form-item--filter',
+  click: function() {
+    $(this)
+      .addClass('active')
+      .siblings()
+      .removeClass('active');
+    $imgs.show();
+  }
+}).appendTo($buttons);
 
+$.each(tagged, function(tagName) {
+  $('<button/>', {
+    text: tagName + ' (' + tagged[tagName].length + ')',
+    class: 'form-item--filter ' + 'cat--' + tagName,
+    click: function () {
+      $(this)
+      .addClass('active')
+      .siblings()
+      .removeClass('active');
+     $imgs
+      .hide()
+      .filter(tagged[tagName])
+      .show();
+    }
+  }).appendTo($buttons);
+});  
+}());  
+});
