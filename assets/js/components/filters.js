8f805faad33f05af203b5c022b75bc769318a7ca
Mark van der Zande <mark.vanderzande@ictu.nl>
2020-12-08 11:46:45 +0100

    filter text

--- assets/js/components/filters.js
+++ assets/js/components/filters.js
@@ -30,8 +30,10 @@ $(document).ready(function(){
   var $works = $('.workItem'); // find the portfolio items
   var showAll = $('.showAll'); // identify the "show all" button
 
-  var cFilter, cFilterData; // declare a variable to store the filter and one for the data to filter by
+  var cFilter, cFilterData, cFiltercat, cFilterCount; // declare a variable to store the filter and one for the data to filter by
   var filtersActive = []; // an array to store the active filters
+  var filtersActiveCat = [];
+  var filtersActiveCount = [];
 
   $filters.keypress(function(e){
     if((e.keyCode ? e.keyCode : e.which) == 13){
@@ -41,7 +43,9 @@ $(document).ready(function(){
 
   $filters.click(function(){ // if filters are clicked
     cFilter = $(this);
-    cFilterData = cFilter.attr('data-filter'); // read filter value
+    cFilterData = cFilter.attr('data-filter');
+    cFiltercat = cFilter.attr('data-categorie');// read filter value
+    cFilterCount = cFilter.attr('data-count');
 
     highlightFilter();
     applyFilter();
@@ -51,15 +55,24 @@ $(document).ready(function(){
     var filterClass = 'filter-active';
     if (cFilter.parent().hasClass(filterClass)) {
       cFilter.parent().removeClass(filterClass);
-      removeActiveFilter(cFilterData);
+      removeActiveFilter(cFilterData,cFiltercat,cFilterCount);
     } else if (cFilter.hasClass('showAll')) {
       $filters.parent().removeClass(filterClass);
-      filtersActive = []; // clear the array
+      filtersActive = [];
+      filtersActiveCat = [];// clear the array
+      filtersActiveCount = [];
       cFilter.parent().addClass(filterClass);
     } else {
       showAll.parent().removeClass(filterClass);
       cFilter.parent().addClass(filterClass);
       filtersActive.push(cFilterData);
+      filtersActiveCat.push(cFiltercat);
+      filtersActiveCount.push(cFilterCount);
+      console.log(filtersActiveCat);
+      $("#category--cards").text(filtersActiveCat.join(", "));
+      sum = 0;
+      $.each(filtersActiveCount,function(){sum+=parseFloat(this) || 0;});
+      $("#count--cards").text(sum);
     }
   }
 
@@ -70,6 +83,8 @@ $(document).ready(function(){
       var classes = $(this).attr('class').split(' ');
       if (cFilter.hasClass('showAll') || filtersActive.length == 0) { // makes sure we catch the array when its empty and revert to the default of showing all items
         $works.addClass('show-workItem'); //show them all
+        $("#category--cards").text(cFiltercat);
+        $("#count--cards").text(cFilterCount);
       } else {
         $(this).removeClass('show-workItem');
         for (i = 0; i < classes.length; i++) {
@@ -82,10 +97,27 @@ $(document).ready(function(){
   }
 
   // remove deselected filters from the ActiveFilter array
-  function removeActiveFilter(item) {
+  function removeActiveFilter(item,cat,count) {
     var index = filtersActive.indexOf(item);
+    var indexCat = filtersActiveCat.indexOf(cat);
+    var indexCount = filtersActiveCount.indexOf(count);
     if (index > -1) {
       filtersActive.splice(index, 1);
+    }
+    if (indexCat > -1) {
+      filtersActiveCat.splice(indexCat, 1);
+      if (filtersActiveCat.length === 0) {
+        $('#cartcount').html("Geen filter geselecteerd");
+      }
+      else{
+      $("#category--cards").text(filtersActiveCat.join(", "));
+      }
+    }
+    if (indexCount > -1) {
+      filtersActiveCount.splice(indexCount, 1);
+      sum = 0;
+      $.each(filtersActiveCount,function(){sum+=parseFloat(this) || 0;});
+      $("#count--cards").text(sum);
     }
   }
 
