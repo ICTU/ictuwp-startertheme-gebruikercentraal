c04f170899fe805a46761dfc264cc5c7e54ab58e
Paul van Buuren <paul@wbvb.nl>
2021-09-27 10:23:58 +0200

    code formatting

--- gutenberg-blocks/gutenberg-settings.php
+++ gutenberg-blocks/gutenberg-settings.php
@@ -12,9 +12,9 @@ add_filter( 'block_categories_all', 'gb_define_categories', 10, 2 );
 
 function gb_define_categories( $categories, $post ) {
 	return array_merge( $categories, [
-			[
-				'slug'  => 'gc-blocks',
-				'title' => _x( 'Gebruiker Centraal', 'Block-categorie', 'gctheme' ),
-			],
-		] );
+		[
+			'slug'  => 'gc-blocks',
+			'title' => _x( 'Gebruiker Centraal', 'Block-categorie', 'gctheme' ),
+		],
+	] );
 }
