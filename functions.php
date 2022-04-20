15ce39d090fd68d5b03eff4a73dc292a91a68d96
David Hund <david.hund@gmail.com>
2022-04-20 10:05:02 +0200

    fix: remove TGM Plugin Activation. We manage plugins through Composer/SatisPress

--- functions.php
+++ functions.php
@@ -65,8 +65,6 @@ if ( $flavor_select == "OD" ) {
 	add_action( 'init', [ 'ICTUWP_GC_OD_registerposttypes', 'init' ], 1 );
 }
 
-// include file for all must-use plugins
-require_once( __DIR__ . '/plugin-activatie/must-use-plugins.php' );
 //========================================================================================================
 /*
  * Extra functionaliteit en filters voor de Events Manager
